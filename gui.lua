c4199d6e4275e2ba8fd58b612afe3c812eeb94d6
seek93 <seek1145@hotmail.com>
2025-03-31 00:21:34 +0800

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,19 @@
+local OrionLib = loadstring(game:HttpGet('https://pastebin.com/raw/xLRUSiKx'))()
+
+local Window = OrionLib:MakeWindow({Name = "storage抽奖相关", HidePremium = false, SaveConfig = true,IntroText = "by seek", ConfigFolder = "FFFFF"})
+local Tab = Window:MakeTab({
+    Name = "功能分类名",
+    Icon = "rbxassetid://4483345998",	
+PremiumOnly = false})
+Tab:AddButton({	Name = "刷钱",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/seek93/the-storage/main/main.lua"))()
+      ()
+    end
+  })
+Tab:AddButton({ Name = "抽奖",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/seek93/the-storage/main/rolling.lua"))()
+      ()
+    end
+  })
